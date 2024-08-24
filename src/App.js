94fcc6c5929e97561822bf92435c4a90438aa284
Krishna Garg <gargkrishna730@gmail.com>
2024-08-24 18:04:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to My React App</h1>
+        <p>
+          This is a sample page created to demonstrate a simple React app CICD.
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
