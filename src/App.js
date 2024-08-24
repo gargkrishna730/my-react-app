48313f152b3195fe398a44dfeb92abb5a07559f9
Krishna Garg <gargkrishna730@gmail.com>
2024-08-24 19:56:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Welcome to My React App</h1>
         <p>
-          This is a sample page created to demonstrate a simple React app CICD using GitHub Actions.
+          This is a sample page created to demonstrate a simple React app CICD using GitHub Actions. Thank You for Attending
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
